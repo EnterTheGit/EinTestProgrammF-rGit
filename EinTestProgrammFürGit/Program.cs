f24a1f1d030f04d56593aa9c06623eeff13ff66a
t.hem <t.hem@outlook.de>
2018-08-21 21:35:25 +0200

    Test des Commit 21.08.2018

---
+++ EinTestProgrammFürGit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinTestProgrammFürGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hallo, ich bin ein Testprogramm");
+            Console.ReadKey();
+        }
+    }
+}
